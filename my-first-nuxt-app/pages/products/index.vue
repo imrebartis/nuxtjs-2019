6925665070bb1fc4f6bc3c3b7e7f25c3e5923a52
Imre Bartis <imre.bartis@siili.com>
2019-02-17 05:05:31 +0200

    validate parameters for dynamic routing

--- my-first-nuxt-app/pages/products/index.vue
+++ my-first-nuxt-app/pages/products/index.vue
@@ -2,10 +2,10 @@
   <div>
     <h1>products</h1>
     <nuxt-link to="/products/1">
-        product 1
+      product 1
     </nuxt-link>
     <nuxt-link to="/products/2">
-        product 2
+      product 2
     </nuxt-link>
   </div>
 </template>
--- my-first-nuxt-app/pages/users/_id/index.vue
+++ my-first-nuxt-app/pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>A single user, with the ID {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    // accept only numbers as id
+    return /^\d+$/.test(data.params.id)
+  }
+}
+</script>
