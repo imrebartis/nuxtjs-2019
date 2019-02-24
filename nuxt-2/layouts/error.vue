096c6c272f48911761ab2d22faffab59102e44f7
Imre Bartis <imre.bartis@siili.com>
2019-02-24 07:21:35 +0200

    set up project

---
+++ nuxt-2/layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>
+    The page you're looking for doesn't exist. Perhaps you'd like to return to the
+    <nuxt-link to="/">
+      homepage
+    </nuxt-link>?
+  </h1>
+</template>
