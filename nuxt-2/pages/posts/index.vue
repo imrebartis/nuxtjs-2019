d1c4d4a06dfbf4d3c440e24ae44170c2d8946ea5
Imre Bartis <imre.bartis@siili.com>
2019-03-13 08:28:03 +0200

    use promises in asyncData

--- nuxt-2/pages/posts/index.vue
+++ nuxt-2/pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -10,6 +10,36 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
+  },
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: '1',
+              title: 'first post',
+              previewText: 'first post yo',
+              thumbnail:
+                'https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg'
+            },
+            {
+              id: '2',
+              title: 'second post',
+              previewText: 'This is the second post!',
+              thumbnail:
+                'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+            }
+          ]
+        })
+      }, 1000)
+    })
+      .then(data => {
+        return data
+      })
+      .catch(e => {
+        context.error(new Error())
+      })
   }
 }
 </script>
