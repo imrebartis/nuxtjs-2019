a2f2d679ba965153f430ecbd34aeddd6b9ebd5f3
Imre Bartis <imre.bartis@siili.com>
2019-03-28 07:50:36 +0200

    add user signup, user login & store the token

--- nuxt-2/store/index.js
+++ nuxt-2/store/index.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      loadedPosts: []
+      loadedPosts: [],
+      token: null
     },
     mutations: {
       setPosts(state, posts) {
@@ -18,6 +19,9 @@ const createStore = () => {
           post => post.id === editedPost.id
         )
         state.loadedPosts[postIndex] = editedPost
+      },
+      setToken(state, token) {
+        state.token = token
       }
     },
     actions: {
@@ -62,6 +66,26 @@ const createStore = () => {
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
+      },
+      authenticateUser(vuexContext, authData) {
+        let authUrl =
+          'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' +
+          process.env.fbAPIKey
+        if (!authData.isLogin) {
+          authUrl =
+            'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' +
+            process.env.fbAPIKey
+        }
+        return axios
+          .post(authUrl, {
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+          })
+          .then(result => {
+            vuexContext.commit('setToken', result.data.idToken)
+          })
+          .catch(e => console.log(e.response.data.error.message))
       }
     },
     getters: {
